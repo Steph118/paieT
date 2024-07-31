f6b4cfe52a6dc4edd27342f80595965d70a951c6
Steph118 <stephaneatsu18@gmail.com>
2024-07-31 23:58:46 +0000

    Members and persons register

--- src/main/java/bean/PersonBean.java
+++ src/main/java/bean/PersonBean.java
@@ -6,6 +6,8 @@ package bean;
 
 import entities.Address;
 import entities.Country;
+import entities.Department;
+import entities.Eglise;
 import entities.Locality;
 import entities.Person;
 import entities.Role;
@@ -19,11 +21,15 @@ import java.util.List;
 import java.util.Objects;
 import org.apache.commons.lang3.StringUtils;
 import service.interfaces.CountryServiceLocal;
+import service.interfaces.DepartmentServiceLocal;
+import service.interfaces.EgliseServiceLocal;
 import service.interfaces.GenericServiceLocal;
 import service.interfaces.LocalityServiceLocal;
 import service.interfaces.PersonServiceLocal;
 import service.interfaces.SexeServiceLocal;
 import service.interfaces.TypeLocalityServiceLocal;
+import services.DepartmentService;
+import services.EgliseService;
 
 /**
  * @author steph18
@@ -42,14 +48,18 @@ public class PersonBean extends GenericBean<Person, Integer> {
     private CountryServiceLocal countryService;
     @EJB
     private SexeServiceLocal sexeService;
+    @EJB
+    private DepartmentServiceLocal departmentService;
+    @EJB
+    private EgliseServiceLocal egliseService;
 
     private Country country;
     private Address address;
     private TypeLocality typeLocality;
     private boolean addUser;
 
-    private List<Country> countries = new ArrayList<>();
-    private List<TypeLocality> typeLocalities = new ArrayList<>();
+    //private List<Country> countries = new ArrayList<>();
+    //private List<TypeLocality> typeLocalities = new ArrayList<>();
     private List<Locality> localities = new ArrayList<>();
 
     @Override
@@ -78,8 +88,8 @@ public class PersonBean extends GenericBean<Person, Integer> {
     @Override
     public void initEntity() {
         super.initEntity();
-        this.countries = this.countryService.getAll();
-        this.typeLocalities = this.typeLocalityService.getAll();
+        ///this.countries = this.countryService.getAll();
+        //this.typeLocalities = this.typeLocalityService.getAll();
     }
 
     @Override
@@ -103,8 +113,24 @@ public class PersonBean extends GenericBean<Person, Integer> {
         return new ArrayList<>();
     }
 
-    public List<Locality> listSexes2() {
-        return localityService.getAll();
+    public List<Sexe> listSexes() {
+        return sexeService.getAll();
+    }
+
+    public List<Eglise> listEglises() {
+        return egliseService.getAll();
+    }
+
+    public List<Department> listDepartements() {
+        return departmentService.getAll();
+    }
+
+    public List<Country> listCountries() {
+        return countryService.getAll();
+    }
+
+    public List<TypeLocality> listTypeLocalities() {
+        return typeLocalityService.getAll();
     }
 
     @Override
@@ -127,14 +153,13 @@ public class PersonBean extends GenericBean<Person, Integer> {
         return true;
     }
 
-    public List<Country> getCountries() {
-        return countries;
-    }
-
-    public List<TypeLocality> getTypeLocalities() {
-        return typeLocalities;
-    }
-
+//    public List<Country> getCountries() {
+//        return countries;
+//    }
+//
+//    public List<TypeLocality> getTypeLocalities() {
+//        return typeLocalities;
+//    }
     public List<Locality> getLocalities() {
         return localities;
     }
