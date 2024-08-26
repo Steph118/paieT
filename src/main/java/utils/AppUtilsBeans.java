d9915725d27ff66e4fa29b05e661659da84a93eb
Steph18 <stephaneatsu18@gmail.com>
2024-08-26 11:51:20 +0000

    Person add review finish

---
+++ src/main/java/utils/AppUtilsBeans.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+/**
+ *
+ * @author steph18
+ */
+@Named
+@ViewScoped
+public class AppUtilsBeans implements Serializable {
+
+    public String numberFormat(BigDecimal somme) {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        try {
+            return (somme != null) ? numberFormat.format(somme) : "";
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
