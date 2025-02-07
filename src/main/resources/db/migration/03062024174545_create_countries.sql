696bfe9f8a822031476bfe59a81ffed179c6679f
Steph18 <katsu@mediasofthome.com>
2025-02-07 19:02:39 +0000

    Flywdb migration

--- src/main/resources/db/migration/03062024174545_create_countries.sql
+++
@@ -1,9 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  steph18
- * Created: 3 Jul 2024
- */
-
