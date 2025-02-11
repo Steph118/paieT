a026d6a77fb61c9ec7ed34cfa5887ef922c94ee0
Steph18 <stephaneatsu18@gmail.com>
2025-02-11 00:45:27 +0000

    rebasing

--- src/main/java/apps/AuthentificationIdentityStore.java
+++ src/main/java/apps/AuthentificationIdentityStore.java
@@ -32,7 +32,6 @@ public class AuthentificationIdentityStore implements IdentityStore {
                     roles
             );
         }
-
         return CredentialValidationResult.INVALID_RESULT;
     }
 
